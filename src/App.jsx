97060fbe7af27f8e75cb7a608de0d39a2dce320a
mahdi <mouffokmed5@gmail.com>
2026-04-27 10:03:46 +0100

    adding the primary and secondary color for the theme

--- src/App.jsx
+++ src/App.jsx
@@ -9,73 +9,45 @@ import Hero from "./component/Hero";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#ff5252",
-    },
-  },
-});
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: "dark",
+//     primary: {
+//       main: "#ff5252",
+//     },
+//   },
+// });
 // use the default theme customasation to switch mode
-// const primary = {
-//   main: "#1976d2",
-//   light: "#42a5f5",
-//   dark: "#1565c0",
-//   contrastText: "#fff",
-// };
+const primary = {
+  main: "#1976d2",
+  light: "#42a5f5",
+  dark: "#1565c0",
+  contrastText: "#fff",
+};
+const secondary = {
+  main: "#1976d2",
+  light: "#42a5f5",
+  dark: "#1565c0",
+  contrastText: "#fff",
+};
 
 //////////////////////////////////::
 
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import { lime, purple } from '@mui/material/colors';
-// import Button from '@mui/material/Button';
 
-// const theme = createTheme({
-//   palette: {
-//     primary: lime,
-//     secondary: purple,
-//   },
-// });
+const theme = createTheme({
+  palette: {
+    primary: primary,
+    secondary: secondary,
+  },
+});
 
-// export default function UsingColorObject() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Button variant="contained">Primary</Button>
-//       <Button variant="contained" color="secondary" sx={{ ml: 2 }}>
-//         Secondary
-//       </Button>
-//     </ThemeProvider>
-//   );
-// }
-
-////////////////////////////////:::::
-
-// import { createTheme } from "@mui/material/styles";
-
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: "#FF5733",
-//       // light: will be calculated from palette.primary.main,
-//       // dark: will be calculated from palette.primary.main,
-//       // contrastText: will be calculated to contrast with palette.primary.main
-//     },
-//     secondary: {
-//       main: "#E0C2FF",
-//       light: "#F5EBFF",
-//       // dark: will be calculated from palette.secondary.main,
-//       contrastText: "#47008F",
-//     },
-//   },
-// });
 
 ///////////////////////////////////////::
 
 function App() {
   return (
     <div className="items-center ">
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <NavBar />
         <Hero />
